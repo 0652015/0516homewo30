a7fd30123b3efecc93d027d3a975313e90d62107
0652015 <ilove2426you@gmail.com>
2018-05-30 22:22:31 +0800

    a non-constant pointer to constant data

--- Untitled2.c
+++ Untitled2.c
@@ -1,14 +1,22 @@
-void bubbleSort(int * const a){
+void bubbleSort(const int  *a){
 	int i,j,temp;
+	int f[5];
+	for(i=0;i<5;i++){
+		f[i]=a[i];
+	}
 	for(i=0;i<5;i++) {
 		for(j=0;j<4;j++){
 			
-		if (a[j]>a[j+1]){
-			temp=a[j];
-			a[j]=a[j+1];
-			a[j+1]=temp;
+		if (f[j]>f[j+1]){
+			temp=f[j];
+			f[j]=f[j+1];
+			f[j+1]=temp;
 		}
 	}
 	}
 	
+printf ("\n2.=");
+	for(i=0;i<5;i++){
+		printf (" %d ",f[i]);
+	}
 }
